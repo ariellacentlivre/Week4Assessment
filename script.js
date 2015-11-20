367f5b8370fdd9c184c66f2b6f530745e28d8a4e
Ariella Centlivre <ariellacentlivre@gmail.com>
2015-11-20 14:20:05 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+(function(){	
+	
+	var animals = [
+		{name:"cow", sound:"moo"},
+		{name:"sheep", sound: "bash"},
+		{name:"dog", sound:"arf"}
+	];
+
+	function seeAndSay () {
+		animals.forEach(function(animal){
+		 animals.gray=“gray”;
+		 console.log("I am a "+.name+" , I make the sound"+.sound+"my coat is"+.gray);
+		}
+	};
+})();
+
+	seeAndSay(animals);
